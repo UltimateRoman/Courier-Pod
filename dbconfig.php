82d83e1fb024a146b5e6abd40c35bf35f5dc4934
Athul Sanjose <59477486+UltimateRoman@users.noreply.github.com>
2021-05-08 20:32:40 +0530

    Initial Commit

---
+++ dbconfig.php
@@ -0,0 +1,3 @@
+<?php
+$dbconn = mysqli_connect("localhost","username","password","courierpod") or die(mysqli_error()); 
+?>
---
+++ login.php
@@ -0,0 +1,20 @@
+<?php
+include "dbconfig.php";
+
+if(isset($_POST['login'])) {
+
+}
+
+else {
+?>
+<html>
+<h1 style="text-align: center">Courier-Pod</h1>
+<h2 style="text-align: center">Admin Login</h2>
+
+<table>
+
+</table>
+</html>
+<?php
+}
+?>
